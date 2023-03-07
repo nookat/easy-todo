d1267745214b6ed51758b3a202b06441efb5057c
Alexander Sosov <nookat.develop@gmail.com>
2023-03-07 10:53:20 +0300

    feat: подключить scss/sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Template</p>
-    </div>
-  );
-}
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <p>Template</p>
+  </div>
+);
 
 export default App;

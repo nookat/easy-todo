fb8e127fce62f3db67f05434169cf2eef48ba631
Alexander Sosov <nookat.develop@gmail.com>
2023-03-10 10:30:47 +0300

    chore: подключить иконку логотипа

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {ReactComponent as LogoIcon} from '../../assets/img/logo.svg';
+
+import {Container} from '../../layouts';
 
 const Header = () => (
   <div className="Header">
-    <p>header</p>
+    <Container>
+      <LogoIcon/>
+      <p>header</p>
+    </Container>
   </div>
 );
 

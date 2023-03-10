a36289f08c91b72548d37fbb2917a94145567ce7
Alexander Sosov <nookat.develop@gmail.com>
2023-03-10 10:30:24 +0300

    chore: создать layout container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './scss/main.scss';
 
 import {Header, Footer} from './components';
+import {Container} from './layouts';
 
 const App = () => (
-  <div className="App">
+  <div className="ui-wrapper">
     <Header/>
-    <p>Template</p>
+    <div className="ui-content-wrapper">
+      <Container>
+        <p className="text-2xl">Template</p>
+      </Container>
+    </div>
     <Footer/>
   </div>
 );

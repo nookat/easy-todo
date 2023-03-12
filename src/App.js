6bd497c33bab793c874e1daa56bb40641384ac17
Alexander Sosov <nookat.develop@gmail.com>
2023-03-12 10:10:12 +0300

    chore: добавить возможность propTypes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,121 @@ import {Container} from './layouts';
 
 const App = () => (
   <div className="ui-wrapper">
-    <Header/>
+    <Header isLogo/>
     <div className="ui-content-wrapper">
       <Container>
         <p className="text-2xl">Template</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus eius ipsam iure quibusdam. Earum
+          facilis illo nulla possimus? A architecto cum cumque dignissimos dolorum ea eligendi, enim esse
+          exercitationem
+          harum hic id illo impedit iste labore magnam, magni necessitatibus non nostrum officia provident quia quis
+          quod ratione soluta tempora temporibus ullam ut vel veritatis vitae voluptate voluptatem, voluptates!
+          Aliquam
+          blanditiis, dignissimos iusto maxime nam non quam sint tempora? A aut dolor dolores ea est eveniet ex facere
+          iure laboriosam libero maiores natus nihil nisi non numquam odit, omnis optio quaerat quisquam rem repellat,
+          repudiandae sunt totam veniam voluptatibus. Adipisci alias amet architecto at dolorem, dolorum eum fuga
+          libero
+          nostrum, numquam officiis perspiciatis porro, provident qui quis quisquam recusandae suscipit totam! Ab
+          dignissimos, dolorum eos et excepturi explicabo minima saepe! Aperiam autem debitis ducimus facere harum
+          impedit inventore, itaque iure laboriosam officia optio quam quia quidem quod repudiandae sapiente similique
+          totam. Accusamus atque dolores earum expedita id nam, officiis optio suscipit. Aliquam, asperiores, dolor,
+          est
+          et expedita fugiat illum ipsa nihil nobis non optio quae quibusdam quidem quo rem repellat ut! Architecto
+          cupiditate dolorem incidunt iusto nostrum. At aut consequuntur culpa delectus dicta eveniet modi quidem quos
+          similique vero? Aliquid commodi deleniti dolorum ex, facilis in ipsam magnam nobis odit omnis perferendis
+          quia
+          quis quisquam tempora totam voluptatibus voluptatum. Ad alias amet aspernatur commodi corporis cumque,
+          delectus deleniti dicta dolores earum eum expedita facere id illum impedit itaque laboriosam nam nemo
+          nesciunt
+          nisi odit officiis qui repellat sed sequi soluta tempora vitae. Amet in incidunt labore perferendis
+          voluptatum. Aperiam at consequuntur cum dolorum eveniet exercitationem hic in nemo nihil nulla officiis
+          optio,
+          praesentium quam rem sint sunt tempore tenetur totam velit vitae? Eos neque officia vel vitae voluptatum.
+          Commodi doloremque illo illum itaque minima nulla numquam quod repudiandae. Assumenda aut cumque dolores
+          itaque magnam nisi non, pariatur totam vero! Accusamus dolore ea eaque eius, fuga incidunt itaque, neque
+          nobis
+          optio pariatur qui quidem quisquam sunt ut, vel vero vitae. A accusantium aspernatur at consequuntur
+          delectus,
+          distinctio dolor doloribus eligendi eos est et eum hic impedit itaque labore maxime molestiae nam nihil non
+          obcaecati porro quam quis repellendus rerum suscipit temporibus velit voluptatibus. Adipisci animi
+          architecto
+          aspernatur cumque dolorum ducimus, enim hic ipsa itaque laborum magni molestiae nam neque nulla pariatur
+          quaerat quia ratione rerum tempore voluptatibus. Aspernatur dolore dolorem, facilis, id itaque iusto
+          laudantium maxime, odit quaerat rem repellat voluptates.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus eius ipsam iure quibusdam. Earum
+          facilis illo nulla possimus? A architecto cum cumque dignissimos dolorum ea eligendi, enim esse
+          exercitationem
+          harum hic id illo impedit iste labore magnam, magni necessitatibus non nostrum officia provident quia quis
+          quod ratione soluta tempora temporibus ullam ut vel veritatis vitae voluptate voluptatem, voluptates!
+          Aliquam
+          blanditiis, dignissimos iusto maxime nam non quam sint tempora? A aut dolor dolores ea est eveniet ex facere
+          iure laboriosam libero maiores natus nihil nisi non numquam odit, omnis optio quaerat quisquam rem repellat,
+          repudiandae sunt totam veniam voluptatibus. Adipisci alias amet architecto at dolorem, dolorum eum fuga
+          libero
+          nostrum, numquam officiis perspiciatis porro, provident qui quis quisquam recusandae suscipit totam! Ab
+          dignissimos, dolorum eos et excepturi explicabo minima saepe! Aperiam autem debitis ducimus facere harum
+          impedit inventore, itaque iure laboriosam officia optio quam quia quidem quod repudiandae sapiente similique
+          totam. Accusamus atque dolores earum expedita id nam, officiis optio suscipit. Aliquam, asperiores, dolor,
+          est
+          et expedita fugiat illum ipsa nihil nobis non optio quae quibusdam quidem quo rem repellat ut! Architecto
+          cupiditate dolorem incidunt iusto nostrum. At aut consequuntur culpa delectus dicta eveniet modi quidem quos
+          similique vero? Aliquid commodi deleniti dolorum ex, facilis in ipsam magnam nobis odit omnis perferendis
+          quia
+          quis quisquam tempora totam voluptatibus voluptatum. Ad alias amet aspernatur commodi corporis cumque,
+          delectus deleniti dicta dolores earum eum expedita facere id illum impedit itaque laboriosam nam nemo
+          nesciunt
+          nisi odit officiis qui repellat sed sequi soluta tempora vitae. Amet in incidunt labore perferendis
+          voluptatum. Aperiam at consequuntur cum dolorum eveniet exercitationem hic in nemo nihil nulla officiis
+          optio,
+          praesentium quam rem sint sunt tempore tenetur totam velit vitae? Eos neque officia vel vitae voluptatum.
+          Commodi doloremque illo illum itaque minima nulla numquam quod repudiandae. Assumenda aut cumque dolores
+          itaque magnam nisi non, pariatur totam vero! Accusamus dolore ea eaque eius, fuga incidunt itaque, neque
+          nobis
+          optio pariatur qui quidem quisquam sunt ut, vel vero vitae. A accusantium aspernatur at consequuntur
+          delectus,
+          distinctio dolor doloribus eligendi eos est et eum hic impedit itaque labore maxime molestiae nam nihil non
+          obcaecati porro quam quis repellendus rerum suscipit temporibus velit voluptatibus. Adipisci animi
+          architecto
+          aspernatur cumque dolorum ducimus, enim hic ipsa itaque laborum magni molestiae nam neque nulla pariatur
+          quaerat quia ratione rerum tempore voluptatibus. Aspernatur dolore dolorem, facilis, id itaque iusto
+          laudantium maxime, odit quaerat rem repellat voluptates.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus eius ipsam iure quibusdam. Earum
+          facilis illo nulla possimus? A architecto cum cumque dignissimos dolorum ea eligendi, enim esse
+          exercitationem
+          harum hic id illo impedit iste labore magnam, magni necessitatibus non nostrum officia provident quia quis
+          quod ratione soluta tempora temporibus ullam ut vel veritatis vitae voluptate voluptatem, voluptates!
+          Aliquam
+          blanditiis, dignissimos iusto maxime nam non quam sint tempora? A aut dolor dolores ea est eveniet ex facere
+          iure laboriosam libero maiores natus nihil nisi non numquam odit, omnis optio quaerat quisquam rem repellat,
+          repudiandae sunt totam veniam voluptatibus. Adipisci alias amet architecto at dolorem, dolorum eum fuga
+          libero
+          nostrum, numquam officiis perspiciatis porro, provident qui quis quisquam recusandae suscipit totam! Ab
+          dignissimos, dolorum eos et excepturi explicabo minima saepe! Aperiam autem debitis ducimus facere harum
+          impedit inventore, itaque iure laboriosam officia optio quam quia quidem quod repudiandae sapiente similique
+          totam. Accusamus atque dolores earum expedita id nam, officiis optio suscipit. Aliquam, asperiores, dolor,
+          est
+          et expedita fugiat illum ipsa nihil nobis non optio quae quibusdam quidem quo rem repellat ut! Architecto
+          cupiditate dolorem incidunt iusto nostrum. At aut consequuntur culpa delectus dicta eveniet modi quidem quos
+          similique vero? Aliquid commodi deleniti dolorum ex, facilis in ipsam magnam nobis odit omnis perferendis
+          quia
+          quis quisquam tempora totam voluptatibus voluptatum. Ad alias amet aspernatur commodi corporis cumque,
+          delectus deleniti dicta dolores earum eum expedita facere id illum impedit itaque laboriosam nam nemo
+          nesciunt
+          nisi odit officiis qui repellat sed sequi soluta tempora vitae. Amet in incidunt labore perferendis
+          voluptatum. Aperiam at consequuntur cum dolorum eveniet exercitationem hic in nemo nihil nulla officiis
+          optio,
+          praesentium quam rem sint sunt tempore tenetur totam velit vitae? Eos neque officia vel vitae voluptatum.
+          Commodi doloremque illo illum itaque minima nulla numquam quod repudiandae. Assumenda aut cumque dolores
+          itaque magnam nisi non, pariatur totam vero! Accusamus dolore ea eaque eius, fuga incidunt itaque, neque
+          nobis
+          optio pariatur qui quidem quisquam sunt ut, vel vero vitae. A accusantium aspernatur at consequuntur
+          delectus,
+          distinctio dolor doloribus eligendi eos est et eum hic impedit itaque labore maxime molestiae nam nihil non
+          obcaecati porro quam quis repellendus rerum suscipit temporibus velit voluptatibus. Adipisci animi
+          architecto
+          aspernatur cumque dolorum ducimus, enim hic ipsa itaque laborum magni molestiae nam neque nulla pariatur
+          quaerat quia ratione rerum tempore voluptatibus. Aspernatur dolore dolorem, facilis, id itaque iusto
+          laudantium maxime, odit quaerat rem repellat voluptates.</p>
       </Container>
     </div>
     <Footer/>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {ReactComponent as LogoIcon} from '../../assets/img/logo.svg';
 
 import {Container} from '../../layouts';
 
-const Header = () => (
-  <div className="Header">
+const Header = ({ isLogo, isFixed }) => {
+  return (
     <Container>
-      <LogoIcon/>
-      <p>header</p>
+      <div className={`Header flex justify-between py-2 mb-4 ${isFixed && 'isFixed'}`}>
+        <div className="Logo">
+          {isLogo && <LogoIcon/>}
+          <span>React app</span>
+        </div>
+        <ul className="ui-button-group">
+          <li className="ui-link">Home</li>
+          <li className="ui-link">About</li>
+        </ul>
+      </div>
     </Container>
-  </div>
-);
+  );
+};
+
+Header.propTypes = {
+  isLogo: PropTypes.bool,
+  isFixed: PropTypes.bool
+};
+
+Header.defaultProps = {
+  isLogo: false,
+  isFixed: true
+};
 
 export default Header;
